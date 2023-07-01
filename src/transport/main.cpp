0f6b74247f4616068b6483b2acdb572e179efa7e
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2023-07-02 00:26:15 +0100

    modified:   CMakeList.txt 	modified:   main.cpp

--- src/transport/main.cpp
+++ src/transport/main.cpp
@@ -2,12 +2,24 @@
 #include <binding/http/server.h>
 #include <binding/http/router.h>
 
+/// proof of concept of the api that we need to build.
 
 int main() {
     binding:http::http_context context;
+    context.port = 9000;
+    context.tls = true;
+    context.host = "127.0.0.1";
+
     auto router = binding::http::make_router()
-    router.add(binding::, "/", [](binding::http::response_writer& writer, binding::http::request&,  binding::http::params params){
+    router.GET("/", [](binding::http::response_writer* writer, binding::http::request*,  binding::http::params& params){
+    });
+    router.GET("/get", [](binding::http::response_writer* resp, binding::http::request* req) {
+        resp->json["origin"] = req->client_addr.ip;
+        resp->json["url"] = req->url;
+        resp->json["args"] = req->query_params;
+        resp->json["headers"] = req->headers;
+        return 200;
     });
     auto server = binding::http::make_server(context, router);
-    server.listen_serve()   
+    server->listen_serve()   
 }
